aed6525e98bd8267543a0e7fe45b6cc6b490e0b3
RAHUL A P <rahul.ap@aot-technologies.com>
2025-11-25 13:28:23 +0530

    fixed import meta issue in prod

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,10 @@
+
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_EMAILJS_SERVICE_ID: string;
   readonly VITE_EMAILJS_TEMPLATE_ID: string;
   readonly VITE_EMAILJS_PUBLIC_KEY: string;
-  // Add other env vars here if needed
 }
 
 interface ImportMeta {
